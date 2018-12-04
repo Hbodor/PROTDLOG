3a87790d83d10885e0e8e8a383052b7df2e76380
ChihebEdine <38221399+ChihebEdine@users.noreply.github.com>
2018-12-04 21:09:08 +0100

    Update ScriptTest.js

--- ScriptTest.js
+++ ScriptTest.js
@@ -5,4 +5,9 @@ var grid=new Grid(nRows0,nColumns0);
 var s = new status_bar(100,100);
 make_game_zone(s,grid);
 
-
+let p = new Pawn(5,5);
+let b = new Bishop(0,0);
+put(p, grid);
+put(b, grid);
+console.log(p.moves(grid));
+console.log(b.moves(grid));
