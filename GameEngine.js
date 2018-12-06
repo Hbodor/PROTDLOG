523096b211d8411fb8e733c5d0c948d08b0217f1
ChihebEdine <38221399+ChihebEdine@users.noreply.github.com>
2018-12-06 19:06:38 +0100

    Update GameEngine.js

--- GameEngine.js
+++ GameEngine.js
@@ -1,3 +1,8 @@
 // this is where the game is set up 
 
 
+// Creating the grid and the status bar
+var grid = new Grid(nRows0,nColumns0);
+var s = new status_bar(100);
+//showing them inside the page
+make_game_zone(s,grid);
