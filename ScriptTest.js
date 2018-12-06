b2174e65450ad2e62e5ff380cc20783a53d670ae
ChihebEdine <38221399+ChihebEdine@users.noreply.github.com>
2018-12-06 19:09:38 +0100

    Update ScriptTest.js

--- ScriptTest.js
+++ ScriptTest.js
@@ -1,14 +1,8 @@
 // all the test should be in this file 
-
-var grid=new Grid(nRows0,nColumns0);
-
-var s = new status_bar(100,100);
-make_game_zone(s,grid);
-
 let p = new Pawn(5,5);
 let b = new Queen(3,3);
 put(p, grid);
 put(b, grid);
 console.log(p.moves(grid));
 console.log(b.moves(grid));
-show_moves(b, grid);
+show_moves(b, grid, 'rgb(160,160,160)');
